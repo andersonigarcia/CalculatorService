fd4f1ba5f19c25852e636dcb9bfcfcb9259ff066
Anderson_analista <andersongarcia.analista@gmail.com>
2021-01-08 00:47:28 -0300

    Test project creation

---
+++ CalculatorServiceAPITests/DivisoresPrimos/Handler/GetDividersAndCousinsNumberQueryHandlerTests.cs
@@ -0,0 +1,36 @@
+﻿using CalculatorService.Application.Calculadora.Queries;
+using CalculatorService.Application.Calculadora.Queries.Responses;
+using MediatR;
+using Moq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CalculatorServiceAPITests.Divisores.Handler
+{
+    public class GetDividersAndCousinsNumberQueryHandlerTests
+    {
+        public GetDividersAndCousinsNumberQueryHandlerTests()
+        {
+            _mediator = new Mock<IMediator>();
+        }
+
+        private Mock<IMediator> _mediator { get; set; }        
+
+        [Fact(DisplayName = "Obter divisores e primos com erro")]
+        public async Task Handler_Erro()
+        {
+            var number = -45;
+            _mediator.Setup(m => m.Send(It.IsAny<GetDividersAndCousinsNumberQuery>(), new CancellationToken()))
+                .ReturnsAsync(new DividersAndCousinsNumberResponse(number));
+
+            var command = new GetDividersAndCousinsNumberQuery(number);
+
+            var result = await _mediator.Object.Send(command);
+
+            Assert.IsType<DividersAndCousinsNumberResponse>(result);
+            Assert.Empty(result.Dividers);
+            Assert.Empty(result.Cousins);
+        }
+    }
+}
---
+++ CalculatorServiceAPITests/Primos/Handler/GetPrimosQueryHandlerTests.cs
@@ -0,0 +1,52 @@
+﻿using CalculatorService.Application.Calculadora.Queries;
+using CalculatorService.Application.Calculadora.Queries.Responses;
+using MediatR;
+using Moq;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CalculatorServiceAPITests.Divisores.Handler
+{
+    public class GetPrimosQueryHandlerTests
+    {
+        public GetPrimosQueryHandlerTests()
+        {
+            _mediator = new Mock<IMediator>();
+        }
+
+        private Mock<IMediator> _mediator { get; set; }
+
+        [Fact(DisplayName = "Obter primos com sucesso")]
+        public async Task Handler_Sucesso()
+        {
+            var numbers = new List<int>() { 1, 3, 5, 9, 15, 45 };
+            var expected = new List<int>() { 1, 3, 5 };
+            _mediator.Setup(m => m.Send(It.IsAny<GetCousinsQuery>(), new CancellationToken()))
+                .ReturnsAsync(new CousinsResponse(numbers));
+
+            var command = new GetCousinsQuery(numbers);
+
+            var result = await _mediator.Object.Send(command);
+
+            Assert.IsType<CousinsResponse>(result);
+            Assert.Equal(expected, result.Cousins);
+        }
+
+        [Fact(DisplayName = "Obter primos com erro")]
+        public async Task Handler_Erro()
+        {
+            var numbers = new List<int>() { -1, -3 };
+            _mediator.Setup(m => m.Send(It.IsAny<GetCousinsQuery>(), new CancellationToken()))
+                .ReturnsAsync(new CousinsResponse(numbers));
+
+            var command = new GetCousinsQuery(numbers);
+
+            var result = await _mediator.Object.Send(command);
+
+            Assert.IsType<CousinsResponse>(result);
+            Assert.Empty(result.Cousins);
+        }
+    }
+}
